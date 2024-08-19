bcc824c3a31c97fa14e1586da567a470641a8722
20221080030160 <bn6382012@gmail.com>
2024-08-19 14:53:57 -0300

    asd

--- src/as.java
+++ src/as.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class as {
+    public static void main(String[] args) {
+
+        int i;
+
+        double cod, nvpa, natv, mida, mv, matc;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Digite as info");
+        cod = input.nextDouble();
+        nvpa = input.nextDouble();
+        natv = input.nextDouble();
+    }
 }
